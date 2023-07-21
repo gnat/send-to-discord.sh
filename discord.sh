81699db77fd0cc3cdae6d1f25f849e9085286040
Nathaniel Sabanski <sabanski.n@gmail.com>
2023-07-21 07:13:43 -0700

    Create discord.sh

---
+++ discord.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+TITLE=${1:-"Title"}
+MESSAGE=${2:-"Message"}
+COLOR=${3:-"999999"}
+URL=${4:-'https://discord.com/api/webhooks/XXXXXXXXXXXXX/XXXXXXXXXXXXX'}
+# API: https://discord.com/developers/docs/resources/channel#embed-object
+curl "$URL" -H "Content-Type: application/json" -X POST -g --data '{"embeds":[{"title":"'"$TITLE"'","description":"'"$MESSAGE"'","color":"'"$((16#$COLOR))"'"}]}'
